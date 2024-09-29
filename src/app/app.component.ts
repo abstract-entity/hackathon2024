4d42e2f460be575c8417d70f3bcde3cf8be3ba00
Abstract-entity <julien.moisan.56@gmail.com>
2024-09-29 05:23:43 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,56 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements AfterViewChecked {
 
+  prodMode: boolean = true;
+  botName = 'S@vMe';
   #http: HttpClient = inject(HttpClient);
   recognizing = false;
   recognition = new (window as any).webkitSpeechRecognition();
   @ViewChild('message') private messageElement!: ElementRef;
   output: string = '';
+  loading: boolean = false;
   notifications: any[] = [];
+
+  basicPrompt: string = `Tu es un commercial dans le domaine de le transport et la logistique en frais et surgelés. ​
+Tu feras une réponse {{data.length}} avec une tonalité {{data.tonality}} avec des compétences soft skill importantes.​
+
+Tu feras une réponse en prenant en compte que l'entreprise cliente est {{data.client}}.​
+Tu répondras au prompt ci-dessous : 
+{{data.message}}`;
+
+  ragPrompty: string = `Tu es un commercial dans le domaine de le transport et la logistique en frais et surgelés. ​
+Tu feras une réponse {{data.length}} avec une tonalité {{data.tonality}} avec des compétences soft skill importantes.​
+
+Tu feras une réponse en prenant en compte que l'entreprise cliente est {{data.client}}.​
+Prends en compte : 
+- la satisfaction client
+- l'évolution de son chiffre d'affaire sur l'année écoulée
+- le montant des litiges sur l'année écoulée
+- le ratio litige sur sur chiffre d'affaire
+- les derniers échanges avec le client
+Tu répondras au prompt ci-dessous : 
+{{data.message}}`;
+
+//   basicPrompt: string = `Tu es un commercial dans le domaine de le transport et la logistique en frais et surgelés. ​
+// Tu feras une réponse {{data.length}} avec une tonalité {{data.tonality}} avec des compétences soft skill importantes.​
+
+// Tu feras une réponse en prenant en compte que l'entreprise cliente est {{data.client}} et le site est {{data.site}}.​
+// Tu répondras au prompt ci-dessous : 
+// {{data.message}}`;
+
+//   ragPrompty: string = `Tu es un commercial dans le domaine de le transport et la logistique en frais et surgelés. ​
+// Tu feras une réponse {{data.length}} avec une tonalité {{data.tonality}} avec des compétences soft skill importantes.​
+
+// Tu feras une réponse en prenant en compte que l'entreprise cliente est {{data.client}} et le site est {{data.site}}.​
+// Prends en compte : 
+// - la satisfaction client
+// - l'évolution de son chiffre d'affaire sur l'année écoulée
+// - le montant des litiges sur l'année écoulée
+// - le ratio litige sur sur chiffre d'affaire
+// - les derniers échanges avec le client
+// Tu répondras au prompt ci-dessous : 
+// {{data.message}}`;
+
   tonalities: any[] = [
     { id: 'amical', name: 'Amical' },
     { id: 'direct', name: 'Direct' },
@@ -31,9 +75,10 @@ export class AppComponent implements AfterViewChecked {
     { id: 'soutenu', name: 'Soutenu' },
   ];
   lengths: any[] = [
-    { id: 'courte', name: 'Courte' },
-    { id: 'moyenne', name: 'Moyenne' },
-    { id: 'longue', name: 'Longue' }
+    { id: 'bullet-point', name: 'Bullet point' },
+    { id: 'courte', name: 'Short' },
+    { id: '', name: 'Standard' },
+    { id: 'longue', name: 'Long' }
   ];
   models: any[] = [
     { id: '1anthropic.claude-3-5-sonnet-20240620-v1:0', name: 'Claude 3.5' },
@@ -44,22 +89,13 @@ export class AppComponent implements AfterViewChecked {
     { id: 'mistral.mixtral-8x7b-instruct-v0:1', name: 'Mixtral 8x7b' }
   ];
 
+  
+
   form = new FormGroup({
     client: new FormControl('Rana'),
     site: new FormControl('028F - Stef Fidenza'),
     rag: new FormControl(false),
-    prompt: new FormControl(`Tu es un commercial dans le domaine de le transport et la logistique en frais et surgelés. ​
-Tu feras une réponse {{data.length}} avec une tonalité {{data.tonality}} avec des compétences soft skill importantes.​
-
-Tu feras une réponse en prenant en compte que l'entreprise cliente est {{data.client}} et le site est {{data.site}}.​
-Prends en compte : 
-- la satisfaction client
-- l'évolution de son chiffre d'affaire sur l'année écoulée
-- le montant des litiges sur l'année écoulée
-- le ratio litige sur sur chiffre d'affaire
-- les derniers échanges avec le client
-Tu répondras au prompt ci-dessous : 
-{{data.message}}`),
+    prompt: new FormControl(this.basicPrompt),
     message: new FormControl('je dois appliquer une augmentation de 10% aux prestations que je fournis à mon client, comment dois je aborder le sujet avec lui ?'),
     tonality: new FormControl('professionnel'),
     length: new FormControl('longue'),
@@ -71,6 +107,14 @@ Tu répondras au prompt ci-dessous :
 
 
   ngAfterViewChecked(): void {;
+    //when rag value in form change we update the prompt
+    this.form.get('rag')?.valueChanges.subscribe((value) => {
+      if (value) {
+        this.form.get('prompt')?.setValue(this.ragPrompty);
+      } else {
+        this.form.get('prompt')?.setValue(this.basicPrompt);
+      }
+    });
     this.#initRecognition();
   }
 
@@ -78,10 +122,17 @@ Tu répondras au prompt ci-dessous :
     this.recognition.continuous = true;
     this.recognition.interimResults = true;
     this.recognition.lang = 'fr-FR';
+    let timeout: any;
 
     this.recognition.onresult = (event: any) => {
       console.log('result', event.results[event.results.length - 1][0].transcript);
       this.form.get('message')?.setValue(event.results[event.results.length - 1][0].transcript);
+
+      clearTimeout(timeout);
+      timeout = setTimeout(() => { 
+        this.recognition.stop();
+        this.recognizing =false;
+      }, 1000); 
     };
 
     this.recognition.onend = () => {
@@ -95,6 +146,7 @@ Tu répondras au prompt ci-dessous :
   }
 
   sendMessage() {
+    this.loading = true;
     this.notifications.push({
       author: 'You',
       date: new Date(),
@@ -104,17 +156,19 @@ Tu répondras au prompt ci-dessous :
       console.log(data);
       if (data.output.text) {
         this.notifications.push({
-          author: 'Steffy',
+          author: this.botName,
           date: new Date(),
-          msg: data.output.text
+          msg: data.output.text,
+          citations: data.citations
         });
       } else {
         this.notifications.push({
-          author: 'Steffy',
+          author: this.botName,
           date: new Date(),
           msg: data.output.message.content[0].text
         });
       }
+      this.loading = false;
     });
     this.form.get('message')?.setValue('');
   }
